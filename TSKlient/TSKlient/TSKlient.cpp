9e04c651593edb9255f86cb0c82b524c403d1c58
Maciej Stefaniak <48411094+madragonse@users.noreply.github.com>
2019-10-30 14:39:38 +0100

    end

--- TSKlient/TSKlient/TSKlient.cpp
+++ TSKlient/TSKlient/TSKlient.cpp
@@ -17,6 +17,7 @@ void wyswietlLogo()
 |    \ |  _  || |    |    \ | | | || |    |  _  |  | |  | | | ||    / 
 | |\  \| | | || |____| |\  \| |_| || |____| | | |  | |  \ \_/ /| |\ \ 
 \_| \_/\_| |_/\_____/\_| \_/ \___/ \_____/\_| |_/  \_/   \___/ \_| \_| 
+
                                                                       )" << std::endl;
 }
 
@@ -37,12 +38,7 @@ int main()
 	int bytesRecived;
 	WSAData data;
 	WORD ver;
-	/*std::cout << std::endl << std::endl << std::endl << std::endl << std::endl;
-	std::cout << "Klient-> Podaj IP Serwera: ";
-	getline(std::cin, ipAdress);
-	system("cls");
-	wyswietlLogo();
-	ipAdress = "127.0.0.1";*/
+	
 
 	//inicjacja biblioteki
 	ver = MAKEWORD(2, 2);
@@ -75,18 +71,11 @@ int main()
 		getline(std::cin, ipAdress);
 		system("cls");
 		wyswietlLogo();
-		inet_pton(AF_INET, ipAdress.c_str(), &hint.sin_addr);
+		inet_pton(AF_INET, ipAdress.c_str(), &hint.sin_addr); //konwertuje string na numeryczny adres ip
 
 	} while (connect(newSocket, (sockaddr*)&hint, sizeof(hint)) == SOCKET_ERROR);
-	 //konwertuje string na numeryczny adres ip
-	/*result = connect(newSocket, (sockaddr*)&hint, sizeof(hint));
-	if (result == SOCKET_ERROR)
-	{
-		std::cout << "Klient-> Nie mozna polaczyc sie z serwerem! " << std::endl;
-		closesocket(newSocket);
-		WSACleanup();
-		return 3;
-	}*/
+	 
+
 	std::cout << std::endl << std::endl << std::endl << std::endl << "Palaczono z serwerem" << std::endl;
 	//działanie (wysyłanie i odbieranie)
 	do
